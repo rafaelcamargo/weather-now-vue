c5d3271dc7a5766df2cec77dfa2855a63acc40d6
Rafael Camargo <contato@rafaelcamargo.com>
2018-03-18 23:01:30 -0300

    Created row item component

---
+++ src/scripts/base/components/row-item/row-item.js
@@ -0,0 +1,20 @@
+import template from './row-item.html'
+
+export default {
+  name: 'row-item',
+  props: ['label', 'value', 'size'],
+  data(){
+    return {
+      sizeCssClass: ''
+    }
+  },
+  created(){
+    this.setSizeCssClass(this.size);
+  },
+  methods: {
+    setSizeCssClass(size){
+      this.sizeCssClass = size ? `row-item-size-${size}` : '';
+    }
+  },
+  template
+};
