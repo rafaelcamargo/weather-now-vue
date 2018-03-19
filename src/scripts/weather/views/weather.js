f57511372c3f4324ac7a19f1d8deef625950e112
Rafael Camargo <contato@rafaelcamargo.com>
2018-03-18 23:17:26 -0300

    Rendering viewport component on weather view

--- src/scripts/weather/views/weather.js
+++ src/scripts/weather/views/weather.js
@@ -1,6 +1,11 @@
-import template from './weather.html'
+import viewport from '@scripts/base/components/viewport/viewport';
+import template from './weather.html';
 
 export default {
+  name: 'weather',
+  components: {
+    viewport
+  },
   data(){
     return {}
   },
