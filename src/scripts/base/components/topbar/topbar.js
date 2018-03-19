617bd57aeedacd7a1d4d5207a15b2ee1409d7832
Rafael Camargo <contato@rafaelcamargo.com>
2018-03-18 23:16:34 -0300

    Importing topbar styles on topbar component

--- src/scripts/base/components/topbar/topbar.js
+++ src/scripts/base/components/topbar/topbar.js
@@ -1,7 +1,12 @@
+import '@styles/base/topbar.styl';
+import logo from '@scripts/base/components/logo/logo';
 import template from './topbar.html';
 
 export default {
   name: 'topbar',
+  components: {
+    logo
+  },
   data(){
     return {}
   },
