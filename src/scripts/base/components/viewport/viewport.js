c6d110c6b1473837a42624453cedd457c6196c88
Rafael Camargo <contato@rafaelcamargo.com>
2018-03-18 23:16:48 -0300

    Importing viewport styles on viewport component

--- src/scripts/base/components/viewport/viewport.js
+++ src/scripts/base/components/viewport/viewport.js
@@ -1,6 +1,11 @@
+import '@styles/base/viewport.styl';
+import topbar from '@scripts/base/components/topbar/topbar';
 import template from './viewport.html';
 
 export default {
+  components: {
+    topbar
+  },
   name: 'viewport',
   data(){
     return {}
