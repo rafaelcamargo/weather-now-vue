db03f32dea71c19f779f4818f2f5f1bcc492900b
Rafael Camargo <contato@rafaelcamargo.com>
2018-03-20 00:01:07 -0300

    Added weather card panel on weather view

--- src/scripts/weather/views/weather.js
+++ src/scripts/weather/views/weather.js
@@ -1,10 +1,12 @@
 import viewport from '@scripts/base/components/viewport/viewport';
+import weatherCardPanel from '@scripts/weather/components/weather-card-panel/weather-card-panel';
 import template from './weather.html';
 
 export default {
   name: 'weather',
   components: {
-    viewport
+    viewport,
+    weatherCardPanel
   },
   data(){
     return {}
