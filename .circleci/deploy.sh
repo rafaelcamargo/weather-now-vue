af6d586e4d6eec32f343df7e845e1755851540dc
Rafael Camargo <contato@rafaelcamargo.com>
2018-03-21 09:55:19 -0300

    Minor fix on deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,7 +4,7 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$branch" = "master" ]; then
     npm install firebase-tools --unsafe-perm
-    npm run build --env=production
+    npm run build -- --env=production
     ./node_modules/firebase-tools/bin/firebase deploy --token=$FIREBASE_TOKEN --non-interactive
 else
     echo "skipped"
