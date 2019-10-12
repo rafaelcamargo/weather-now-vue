372b8905e5a1de45353448d2859e35d3bae8b914
Rafael Camargo <hello@rafaelcamargo.com>
2019-10-12 19:43:54 -0300

    chore: improve package.json and deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,9 +3,7 @@
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$branch" = "master" ]; then
-    npm install firebase-tools --unsafe-perm
-    npm run build -- --env=production
-    ./node_modules/firebase-tools/bin/firebase deploy --token=$FIREBASE_TOKEN --non-interactive
+    npm run deploy -- --token=$FIREBASE_TOKEN
 else
     echo "skipped"
 fi
